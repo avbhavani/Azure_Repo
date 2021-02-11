19a003e9cfd0a116c835e46e2d0d569a463316a0
avsub <gangabhavani.akepati@gmail.com>
2021-02-11 13:01:21 -0600

    Azure Selenium test

---
+++ src/test/java/sampleAMZ/AmazonTest.java
@@ -0,0 +1,22 @@
+package sampleAMZ;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+	@Test
+
+	public void TestAMZ() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\avsub\\Downloads\\chromedriver_87\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		boolean logo =driver.findElement(By.xpath("//*[@id=\"nav-logo-sprites\"]")).isDisplayed();
+
+		System.out.println("logo display is:"+logo);
+		
+	}
+
+}
